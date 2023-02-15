fec69db2068f83d2f117ddf00e75a3c43e0c6e36
ArturoRubin <116040655+ArturoRubin@users.noreply.github.com>
2023-02-15 12:17:07 -0600

    actualizacion formulario y nuevas vistas

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -6,4 +6,8 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+router.get("/crear-reseña", (req, res, next) => {
+  res.render("creacionReseña");
+});
+
 module.exports = router;
