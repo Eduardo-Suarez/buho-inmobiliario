735e94e22d8d20657775a2fe773484adc5546e8a
Eduardo-Suarez <draco_008@hotmail.com>
2023-02-16 13:32:34 -0800

    actualizacion middle

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ require("./config")(app);
 const capitalize = require("./utils/capitalize");
 const projectName = "buho-inmobiliario";
 
-app.locals.appTitle = `${capitalize(projectName)} created with IronLauncher`;
+app.locals.appTitle = `${capitalize(projectName)}`;
 
 // 👇 Start handling routes here
 const indexRoutes = require("./routes/index.routes");
